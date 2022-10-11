50d8ad9779344572f4b6d1d32ab7cbc33c8176ff
Tiranoe <jungwoo9543@gmail.com>
2022-10-11 15:42:54 -0400

    added the about and home.html

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.views import View 
 # this handles viewing class to handle requests
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
-class Home(View):
-    def get(self, request):
-        return HttpResponse("Travel app home")
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
