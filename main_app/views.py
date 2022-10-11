322518fd9e50fd610d16a7dac1dd9c8837b771b9
Tiranoe <jungwoo9543@gmail.com>
2022-10-11 18:28:33 -0400

    removed Post class in views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import View 
 # this handles viewing class to handle requests
-from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+# import models
+from .models import Post
+
 
 # Create your views here.
 class Home(TemplateView):
@@ -11,18 +13,9 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = "about.html"
 
-class Post:
-    def __init__(self, name, location, image, currency, description):
-        self.name = name
-        self.location = location
-        self.image = image
-        self.currency = currency
-        self.description = description
-
 class PostList(TemplateView):
     template_name = "post_list.html"
-    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["posts"] = posts
+        context["posts"] = Post.objects.all()
         return context
