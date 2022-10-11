e996a12ec2e9802ebbd1f6ccf58ec426b0376bef
Tiranoe <jungwoo9543@gmail.com>
2022-10-11 16:45:21 -0400

    complete templateviews with few data

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
+    path('posts/', views.PostList.as_view(), name="post_list"), 
 ]
