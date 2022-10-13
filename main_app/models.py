a90b83165e5dcde086e804adbf16188cde5555fb
Tiranoe <jungwoo9543@gmail.com>
2022-10-12 20:45:11 -0400

    login.html created

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -10,6 +11,7 @@ class Post(models.Model):
     description = models.TextField(max_length=700)
     highly_recommended = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     
     def __str__(self):
         return self.name
@@ -28,3 +30,5 @@ class Attractions(models.Model):
         return self.thingstodo
     def __str__(self):
         return self.placestovisit
+
+
